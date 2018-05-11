be633bfe7b87e5ccc6c4e2b2ae3e137271b41332
bernhardfrank <bernhard_f@hotmail.com>
2018-05-11 15:32:37 +0200

    initial commit

---
+++ CustomScriptExtensions/InstallHyperV.ps1
@@ -0,0 +1,19 @@
+﻿<# 
+    This Script Extension installs Hyper-v 
+
+#>
+
+#this will be our temp folder - need it for download / logging
+$tmpDir = "c:\temp\" 
+$log = $($tmpDir+"\ScriptExtension.log")
+
+#create folder if it doesn't exist
+if (!(Test-Path $tmpDir)) { mkdir $tmpDir -force}
+
+"I was run at: {0}" -f (Get-Date)  | Out-File -FilePath $log -Append
+
+Start-Transcript $log
+
+Install-WindowsFeature Hyper-V -IncludeAllSubFeature -IncludeManagementTools -Verbose
+
+Stop-Transcript
