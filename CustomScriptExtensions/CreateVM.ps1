d3624f200d3b7d88904142dfbddb4fff1554b45f
bernhardfrank <bernhard_f@hotmail.com>
2018-05-11 17:53:02 +0200

    added note user in createvm.ps1

--- CustomScriptExtensions/CreateVM.ps1
+++ CustomScriptExtensions/CreateVM.ps1
@@ -20,7 +20,7 @@ $VMPath = "$((Get-Volume | Sort-Object sizeRemaining -Descending | Select-Object
 $VmDirectory = "$VMPath\$VMName"
 $VHDDirectory = $VmDirectory+'\Virtual Hard Disks'
 New-Item -Path $VHDDirectory -ItemType Directory
-New-VM -Name $VmName -MemoryStartupBytes 1GB -NewVHDPath $($VHDDirectory+"\"+$VmName+".vhdx") -NewVHDSizeBytes 40GB -Path $VMPath -Generation 2 -SwitchName "NATSwitch" | Set-VM -ProcessorCount 1 -AutomaticStopAction ShutDown -AutomaticStartAction Nothing
+New-VM -Name $VmName -MemoryStartupBytes 1GB -NewVHDPath $($VHDDirectory+"\"+$VmName+".vhdx") -NewVHDSizeBytes 70GB -Path $VMPath -Generation 2 -SwitchName "NATSwitch" | Set-VM -ProcessorCount 2 -AutomaticStopAction ShutDown -AutomaticStartAction Nothing
 
 #locate iso and attach as DVD
 $IsoPath = Get-Item "$tmpDir\*server*.iso" | Select-Object -First 1
@@ -28,5 +28,7 @@ Add-VMDvdDrive -VMName $VmName -Path $($IsoPath.FullName)
 
 #make DVD bootable
 Set-VMFirmware -VMName $VmName -FirstBootDevice $(Get-VMDvdDrive -VMName $VmName)
+
+"your host plays Default GW for the VM -> your Hyper-V VM IP should be something like 172.16.0.x / mask 255.255.255.0 / DG: 172.16.0.1 / DNS e.g.: 8.8.8.8" | Out-File "$tmpDir\VMReadme.txt"
 
 Stop-Transcript
