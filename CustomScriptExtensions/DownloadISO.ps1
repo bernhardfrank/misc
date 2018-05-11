b99de5367d81f5a8f558e0e0d8f4d6a5d76164d3
bernhardfrank <bernhard_f@hotmail.com>
2018-05-11 16:18:01 +0200

    added CSE for downloading stuff

---
+++ CustomScriptExtensions/DownloadISO.ps1
@@ -0,0 +1,37 @@
+﻿<# 
+    This Script Extension downloads a Windows Server Eval Iso
+
+#>
+
+Param
+  (
+    [Parameter(Mandatory=$true,
+               ParameterSetName="WindowsServerURI",
+               HelpMessage="Enter the link to the windows server iso")]
+    [String[]]$URI
+  ) 
+
+#this will be our temp folder - need it for download / logging
+$tmpDir = "c:\temp\" 
+$log = $($tmpDir+"\ScriptExtension.log")
+
+#create folder if it doesn't exist
+if (!(Test-Path $tmpDir)) { mkdir $tmpDir -force}
+
+"I was run at: {0}" -f (Get-Date)  | Out-File -FilePath $log -Append
+
+Start-Transcript $log -Append
+
+$URIPath = $tmpDir + "\$(Split-Path $URI -Leaf)"
+
+
+if (!(Test-Path $URIPath )) 
+{
+    Write-Output "starting download...."
+    start-bitstransfer "$URI" "$URIPath" -Priority High -RetryInterval 60 -Verbose -TransferType Download
+    Write-Output "finished downloading: $URIPath"
+}
+
+Stop-Transcript
+
+
