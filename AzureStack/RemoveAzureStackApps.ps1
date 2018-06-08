e0eea9d0973bbb4255b9e02a6c86f4888e9cadc1
bernhardfrank <bernhard_f@hotmail.com>
2018-06-08 18:03:42 +0200

    cleanup AAD from previous Azure Stack installation

---
+++ AzureStack/RemoveAzureStackApps.ps1
@@ -0,0 +1,11 @@
+﻿<#
+
+    For cleaning up an Azure AD from a previous Azure Stack installation
+
+#>
+
+Login-AzureRmAccount   #login to Azure subscription for the account you used to install Azure Stack
+
+$apps = Get-AzureRmADApplication | where Displayname -Like "Azure*"
+$apps|Set-AzureRmADApplication -AvailableToOtherTenants $false 
+$apps | % {Remove-AzureRmADApplication -ObjectId $_.ObjectId -Force}
